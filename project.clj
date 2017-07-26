f835b7bce79f127ba1a54ee2d356db2fd6bc3e4a
oylenshpeegul <theaney@gmail.com>
2017-07-25 22:41:37 -0400

    FIXME

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject epochs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Convert various epoch times to java-time/instant times"
+  :url "http://oylenshpeegul.github.io/Time-Moment-Epoch/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
