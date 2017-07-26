8f493c6c5ea7a3b83e833369c813980f8d91bd77
oylenshpeegul <theaney@gmail.com>
2017-07-26 08:08:48 -0400

    group tests

--- test/epochs/core_test.clj
+++ test/epochs/core_test.clj
@@ -3,37 +3,59 @@
             [epochs.core :refer :all]))
 
 (deftest apfs-test
-  (testing "apfs 1234567890000000000"
-    (is (= (str (java-time/format (apfs 1234567890000000000))
-                (str "2009-02-13T23:31:30Z"))))))
+  (testing "apfs"
+    (is (= (str (java-time/format (apfs 1234567890000000000)))
+           (str "2009-02-13T23:31:30Z")))
+    (is (= (str (java-time/format (apfs 1234567890123000000)))
+           (str "2009-02-13T23:31:30.123Z")))
+    (is (= (str (java-time/format (apfs 1234567890123456000)))
+           (str "2009-02-13T23:31:30.123Z")))
+    (is (= (str (java-time/format (apfs 1234567890123456789)))
+           (str "2009-02-13T23:31:30.123Z")))))
 
 (deftest chrome-test
-  (testing "chrome 12879041490000000"
-    (is (= (str (java-time/format (chrome 12879041490000000))
-                (str "2009-02-13T23:31:30Z"))))))
+  (testing "chrome"
+    (is (= (str (java-time/format (chrome 12879041490000000)))
+           (str "2009-02-13T23:31:30Z")))
+    (is (= (str (java-time/format (chrome 12879041490123000)))
+           (str "2009-02-13T23:31:30.123Z")))
+    (is (= (str (java-time/format (chrome 12879041490123456)))
+           (str "2009-02-13T23:31:30.123Z")))))
 
 (deftest cocoa-test
-  (testing "cocoa 256260690"
-    (is (= (str (java-time/format (cocoa 256260690))
-                (str "2009-02-13T23:31:30Z"))))))
+  (testing "cocoa"
+    (is (= (str (java-time/format (cocoa 256260690)))
+           (str "2009-02-13T23:31:30Z")))))
 
 (deftest unix-test
-  (testing "unix 1234567890"
-    (is (= (str (java-time/format (unix 1234567890))
-                (str "2009-02-13T23:31:30Z"))))))
+  (testing "unix"
+    (is (= (str (java-time/format (unix 1234567890)))
+           (str "2009-02-13T23:31:30Z")))))
 
 (deftest uuid-v1-test
-  (testing "uuid-v1 134538606900000000"
-    (is (= (str (java-time/format (uuid-v1 134538606900000000))
-                (str "2009-02-13T23:31:30Z"))))))
+  (testing "uuid-v1"
+    (is (= (str (java-time/format (uuid-v1 134538606900000000)))
+           (str "2009-02-13T23:31:30Z")))
+    (is (= (str (java-time/format (uuid-v1 134538606901230000)))
+           (str "2009-02-13T23:31:30.123Z")))
+    (is (= (str (java-time/format (uuid-v1 134538606901234567)))
+           (str "2009-02-13T23:31:30.123Z")))))
 
 (deftest windows-date-test
-  (testing "windows-date 633701646900000000"
-    (is (= (str (java-time/format (windows-date 633701646900000000))
-                (str "2009-02-13T23:31:30Z"))))))
+  (testing "windows-date"
+    (is (= (str (java-time/format (windows-date 633701646900000000)))
+           (str "2009-02-13T23:31:30Z")))
+    (is (= (str (java-time/format (windows-date 633701646901230000)))
+           (str "2009-02-13T23:31:30.123Z")))
+    (is (= (str (java-time/format (windows-date 633701646901234567)))
+           (str "2009-02-13T23:31:30.123Z")))))
 
 (deftest windows-file-test
-  (testing "windows-file 128790414900000000"
-    (is (= (str (java-time/format (windows-file 128790414900000000))
-                (str "2009-02-13T23:31:30Z"))))))
+  (testing "windows-file"
+    (is (= (str (java-time/format (windows-file 128790414900000000)))
+           (str "2009-02-13T23:31:30Z")))
+    (is (= (str (java-time/format (windows-file 128790414901230000)))
+           (str "2009-02-13T23:31:30.123Z")))
+    (is (= (str (java-time/format (windows-file 128790414901234567)))
+           (str "2009-02-13T23:31:30.123Z")))))
 
