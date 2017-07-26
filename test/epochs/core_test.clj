30a2d09088dffe71a507d62ab1b1f957a6774b7e
oylenshpeegul <theaney@gmail.com>
2017-07-25 22:48:26 -0400

    cocoa

--- test/epochs/core_test.clj
+++ test/epochs/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [epochs.core :refer :all]))
 
+(deftest apfs-test
+  (testing "apfs 1234567890000000000"
+    (is (= (str (java-time/format (apfs 1234567890000000000))
+                (str "2009-02-13T23:31:30Z"))))))
+
+(deftest chrome-test
+  (testing "chrome 12879041490000000"
+    (is (= (str (java-time/format (chrome 12879041490000000))
+                (str "2009-02-13T23:31:30Z"))))))
+
+(deftest cocoa-test
+  (testing "cocoa 256260690"
+    (is (= (str (java-time/format (cocoa 256260690))
+                (str "2009-02-13T23:31:30Z"))))))
+
 (deftest unix-test
   (testing "unix 1234567890"
     (is (= (str (java-time/format (unix 1234567890))
