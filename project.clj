419a70775e4fdb7f7413eae193196410df186661
oylenshpeegul <theaney@gmail.com>
2017-07-27 23:39:37 -0400

    remove java-time dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://oylenshpeegul.github.io/Time-Moment-Epoch/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clojure.java-time "0.3.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
+                 
                  
